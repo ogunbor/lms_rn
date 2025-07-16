f3f89f3410d38163a340e24791e20caa5a83dbcd
mompei <ogunborjerry@gmail.com>
2025-07-16 14:08:56 +0100

    created actions.js in redux

---
+++ src/store/actions.js
@@ -0,0 +1,9 @@
+export const setUser = (user) => ({
+    type: "SET_USER",
+    payload: user,
+});
+
+export const setLoading = (loading) => ({
+    type: "SET_LOADING",
+    payload: loading,
+});
