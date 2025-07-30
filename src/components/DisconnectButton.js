4180ae2877c3c8d00d56b95390ecc57f705c0eea
mompei <ogunborjerry@gmail.com>
2025-07-30 10:44:24 +0100

    Added DisconnectButton

---
+++ src/components/DisconnectButton.js
@@ -0,0 +1,36 @@
+import { transact } from '@solana-mobile/mobile-wallet-adapter-protocol-web3js';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+export default function DisconnectButton({ onDisconnect, authorization }) {
+    const onPress = async () => {
+        await transact(async wallet => {
+            await wallet.deauthorize({
+                auth_token: authorization.authToken,
+            });
+            // Set auth token to null
+            onDisconnect();
+        });
+    };
+
+    return (
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Text style={styles.buttonText}>Disconnect Wallet</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: 'red',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+        alignItems: 'center',
+        width: '100%',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+    },
+});
